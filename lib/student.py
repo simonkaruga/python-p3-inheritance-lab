53ac52ce9a4106b9117047d4faa055d88fc3a065
simonkaruga <simonkaruganjoroge@gmail.com>
2025-11-25 09:11:27 +0300

    Add origing

--- lib/student.py
+++ lib/student.py
@@ -5,4 +5,11 @@ from user import User
 class Student(User):
     
     def learn(self):
-        pass
+        return self.knowledge
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+        return
+    def learn(self, subject):
+        self.knowledge.append(subject)
+        return
--- lib/teacher.py
+++ lib/teacher.py
@@ -7,4 +7,18 @@ import random
 class Teacher(User):
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Math",
+            "Science",
+            "History",
+            "Art",
+            "Physical Education",
+            "Music",
+            "Literature",
+            "Biology",
+            "Chemistry",
+            "Physics"
+        ]
